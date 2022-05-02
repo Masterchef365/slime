e77392e36b8744340a82204f2bf0da048081748d
Masterchef365 <duncan.freeman1@gmail.com>
2022-05-02 03:07:57 -0700

    Script

---
+++ script.sh
@@ -0,0 +1,10 @@
+cargo run\
+    --release\
+    --\
+    --decay 0.05\
+    --n-particles 4000\
+    --turn-speed 1.8\
+    --dt 0.5\
+    --move-speed 1.\
+    --sensor-spread 0.5\
+    --sample-dist 1.5
