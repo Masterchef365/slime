5ec9243bf8038add0663d5e352ed52b14aa965fe
Masterchef365 <duncan.freeman1@gmail.com>
2022-05-10 21:59:29 -0700

    Paths direct to PNG

--- src/bin/png.rs
+++ src/bin/png.rs
@@ -1,9 +1,14 @@
 use anyhow::{Context, Result};
 use idek_basics::Array2D;
-use slime::{record::RecordFile, xiaolin::draw_line};
-use std::{path::{PathBuf, Path}, fs::File, io::BufWriter};
-use structopt::StructOpt;
+use nalgebra::Vector2;
 use slime::xiaolin;
+use slime::{record::RecordFile, xiaolin::draw_line};
+use std::{
+    fs::File,
+    io::BufWriter,
+    path::{Path, PathBuf},
+};
+use structopt::StructOpt;
 
 type Rgb = [f32; 3];
 
@@ -32,31 +37,17 @@ struct Opt {
 
     #[structopt(short, long, default_value = "1000")]
     height: usize,
+
+    /// Intensity of plotted points
+    #[structopt(short, long, default_value = "0.01")]
+    intensity: f32,
 }
 
 fn main() -> Result<()> {
     let args = Opt::from_args();
 
-    let mut image = Array2D::new(args.width, args.height);
+    let mut image: Array2D<[f32; 3]> = Array2D::new(args.width, args.height);
 
-    let n = 365;
-    for i in 0..n {
-        let i = i as f32 / n as f32;
-
-        let i = i * std::f32::consts::TAU;
-
-        let x0 = args.width as f32 / 2.;
-        let y0 = args.height as f32 / 2.;
-
-        let r = args.width as f32 / 3.;
-
-        let x1 = r * i.cos() + x0;
-        let y1 = r * i.sin() + y0;
-
-        draw_line(x0, y0, x1, y1, |x, y, b| image[(x as usize, y as usize)] = [b; 3]);
-    }
-
-    /*
     println!("Loading...");
     let record = RecordFile::load(&args.record)?;
 
@@ -67,6 +58,24 @@ fn main() -> Result<()> {
     let first = record.frames.first().context("No frames :/")?;
 
     let mut last = first;
+
+    // Mapping from slime space to PNG space
+    let coord_map = |v: Vector2<f32>| {
+        (
+            v.x * args.width as f32 / record.width as f32,
+            v.y * args.height as f32 / record.height as f32,
+        )
+    };
+
+    // Bounds check before plotting to image (additive)
+    let mut plot_point = |x: i32, y: i32, color: [f32; 3]| {
+        if x >= 0 && y >= 0 && x < args.width as i32 && y < args.height as i32 {
+            image[(x as usize, y as usize)]
+                .iter_mut()
+                .zip(color)
+                .for_each(|(o, i)| *o += i * args.intensity);
+        }
+    };
 
     println!("Building SVG...");
     for (idx, frame) in frames.into_iter().enumerate() {
@@ -80,13 +89,14 @@ fn main() -> Result<()> {
 
         for (part, prev) in frame.slime.iter().zip(&last.slime) {
             if part.age != 0 {
-                todo!()
+                let (x0, y0) = coord_map(prev.position);
+                let (x1, y1) = coord_map(part.position);
+                draw_line(x0, y0, x1, y1, |x, y, b| plot_point(x, y, [b; 3]));
             }
         }
 
         last = frame;
     }
-    */
 
     println!("Writing...");
     let data = rgb8_image(&image);
@@ -119,5 +129,3 @@ fn write_png(path: &Path, data: &[u8], width: u32, height: u32) -> Result<()> {
 
     Ok(())
 }
-
-
