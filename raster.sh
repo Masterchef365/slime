8d39015131dac65ff95d4c3d3cac370870cf3f59
Masterchef365 <duncan.freeman1@gmail.com>
2022-05-20 23:28:46 -0700

    Add a couple scripts

---
+++ raster.sh
@@ -0,0 +1,6 @@
+cargo run --release --bin png --\
+	out.slm\
+	-w 2880\
+	-h 2880\
+    --intensity 0.015
+
---
+++ record.sh
@@ -0,0 +1,4 @@
+cargo run --release -- --record out.slm\
+    --n-particles 8000\
+    -w 1000 -h 1000
+    
